93d6a6b57f07b08c484ef197e1e2b238efbfcbed
Faris Hasim <farishasimsyauqi@gmail.com>
2020-01-18 19:50:56 +0700

    second commit

--- test.c
+++ test.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
 void main() {
-    printf("first massage.\n");
-
-    printf("second message.\n");
-    int i;
-    for(i=0;i<5;i++){
-        printf("a\n");
-    }
+    printf(".\n");
 }
